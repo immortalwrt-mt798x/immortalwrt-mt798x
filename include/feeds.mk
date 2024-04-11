1a574f9da254257187a74d1954f59c62d7c237f9
Ethan Zuo <yuxuan.zuo@outlook.com>
2024-04-12 07:26:22 +0800

    Revert "build: remove useless core feed for mt798x"
    This reverts commit 2828f34bee8d5884e0c2137e0ed236fd5590a818.
    
    Signed-off-by: Ethan Zuo <yuxuan.zuo@outlook.com>

--- include/feeds.mk
+++ include/feeds.mk
@@ -30,8 +30,7 @@ endef
 # 1: destination file
 define FeedSourcesAppend
 ( \
-  $(if $(filter-out mt798%, $(if $(SUBTARGET),$(SUBTARGET),generic)), \
-    echo 'src/gz %d_core %U/targets/%S/packages';, echo -n '';) \
+  echo 'src/gz %d_core %U/targets/%S/packages'; \
   $(strip $(if $(CONFIG_PER_FEED_REPO), \
 	echo 'src/gz %d_base %U/packages/%A/base'; \
 	$(if $(filter %SNAPSHOT-y,$(VERSION_NUMBER)-$(CONFIG_BUILDBOT)), \
